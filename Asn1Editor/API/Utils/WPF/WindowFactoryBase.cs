a5fe0c8d0a698963939ff9991ea7f8a9c3666f85
Vadims Podāns <vpodans@sysadmins.lv>
2023-10-20 12:50:40 +0300

    fixed broken braces

--- Asn1Editor/API/Utils/WPF/WindowFactoryBase.cs
+++ Asn1Editor/API/Utils/WPF/WindowFactoryBase.cs
@@ -1,21 +1,21 @@
 ﻿using System;
 using System.Windows;
 
-namespace SysadminsLV.Asn1Editor.API.Utils.WPF {
-    abstract class WindowFactoryBase {
-        protected Window hwnd;
-        void setParent(Boolean mainWindowAsParent = false) {
-            WindowCollection windows = Application.Current.Windows;
-            Window parent = null;
-            if (windows.Count > 0)
-            {
-                parent = mainWindowAsParent 
-                    ? windows[0] 
-                    : windows[windows.Count - 2];
-            }
+namespace SysadminsLV.Asn1Editor.API.Utils.WPF;
+abstract class WindowFactoryBase {
+    protected Window hwnd;
+    void setParent(Boolean mainWindowAsParent = false) {
+        WindowCollection windows = Application.Current.Windows;
+        Window parent = null;
+        if (windows.Count > 0) {
+            parent = mainWindowAsParent
+                ? windows[0]
+                : windows[windows.Count - 2];
         }
+
         hwnd.Owner = parent;
     }
+
     protected void ShowAsWindow(Boolean mainWindowAsParent = false) {
         setParent(mainWindowAsParent);
         hwnd.Show();
