6bb08ec754c546e0d0d80081a52ae1676a49bdbb
Vadims Podāns <vpodans@sysadmins.lv>
2023-10-20 16:08:54 +0300

    Added global data object to store global data. Currently, it holds app version

---
+++ Asn1Editor/API/ViewModel/GlobalData.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Reflection;
+
+namespace SysadminsLV.Asn1Editor.API.ViewModel;
+class GlobalData {
+    public GlobalData() {
+        AppVersion = Assembly.GetEntryAssembly().GetName().Version.ToString(3);
+    }
+    public String AppVersion { get; set; }
+}
