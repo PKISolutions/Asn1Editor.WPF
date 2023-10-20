0f91cea6608b6d11ce102059385843149cbfeb87
Vadims Podāns <vpodans@sysadmins.lv>
2023-10-20 16:15:51 +0300

    resolved parent window selection based on where the app is running (as normal app or inside the AdornerWindow created by VS

--- Asn1Editor/API/Utils/WPF/WindowFactoryBase.cs
+++ Asn1Editor/API/Utils/WPF/WindowFactoryBase.cs
@@ -1,16 +1,20 @@
 ﻿using System;
+using System.Linq;
 using System.Windows;
 
 namespace SysadminsLV.Asn1Editor.API.Utils.WPF;
 abstract class WindowFactoryBase {
     protected Window hwnd;
     void setParent(Boolean mainWindowAsParent = false) {
-        WindowCollection windows = Application.Current.Windows;
+        Window[] windows = Application.Current.Windows
+            .Cast<Window>()
+            .Where(x => x.ActualWidth != 0)
+            .ToArray();
         Window parent = null;
-        if (windows.Count > 0) {
+        if (windows.Any()) {
             parent = mainWindowAsParent
                 ? windows[0]
-                : windows[windows.Count - 2];
+                : windows[windows.Length - 1];
         }
 
         hwnd.Owner = parent;
