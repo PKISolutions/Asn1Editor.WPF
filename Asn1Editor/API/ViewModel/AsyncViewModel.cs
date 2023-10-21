044d50b57c27d6aa6cd91b51d335de9bc9b0fcb5
Vadims Podāns <vpodans@sysadmins.lv>
2023-10-21 11:54:07 +0300

    added AsyncViewModel

---
+++ Asn1Editor/API/ViewModel/AsyncViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SysadminsLV.Asn1Editor.API.ViewModel;
+public abstract class AsyncViewModel : ViewModelBase {
+    Boolean isBusy;
+
+    public Boolean IsBusy {
+        get => isBusy;
+        set {
+            isBusy = value;
+            OnPropertyChanged(nameof(IsBusy));
+        }
+    }
+}
