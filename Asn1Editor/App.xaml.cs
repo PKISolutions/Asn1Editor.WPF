941c598d8c6cb0fef13b439c249806b1c60b4571
Vadims Podāns <vpodans@sysadmins.lv>
2023-10-24 11:53:23 +0300

    removed IDataSource DI registration

--- Asn1Editor/App.xaml.cs
+++ Asn1Editor/App.xaml.cs
@@ -102,7 +102,6 @@ public partial class App {
 
         Container.RegisterType<IWindowFactory, WindowFactory>();
         Container.RegisterType<IAppCommands, AppCommands>();
-        Container.RegisterSingleton<IDataSource, DataSource>();
         Container.RegisterType<ITagDataEditor, TagDataEditor>();
         // view models
         Container.RegisterSingleton<MainWindowVM>();
