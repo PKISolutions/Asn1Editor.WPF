c7c23895c312ebab7b9e54fd0df431ba0506fd05
Vadims Podāns <vpodans@sysadmins.lv>
2024-08-27 10:54:21 +0300

    "new tree node" user control refactoring.

---
+++ Asn1Editor/Views/UserControls/NewTreeNodeFormUC.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SysadminsLV.Asn1Editor.Views.UserControls;
+/// <summary>
+/// Interaction logic for NewTreeNodeFormUC.xaml
+/// </summary>
+public partial class NewTreeNodeFormUC : UserControl {
+    public NewTreeNodeFormUC() {
+        InitializeComponent();
+    }
+}
