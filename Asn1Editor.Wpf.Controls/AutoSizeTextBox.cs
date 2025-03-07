44645470bae8f5e55446f3e949a8990f87e08174
Vadims Podāns <vpodans@sysadmins.lv>
2025-03-07 09:53:41 +0200

    Drafted new AutoSizeTextBox control

---
+++ Asn1Editor.Wpf.Controls/AutoSizeTextBox.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using Asn1Editor.Wpf.Controls.Helpers;
+
+namespace Asn1Editor.Wpf.Controls;
+
+/// <summary>
+/// Represents a textbox control that can set its width to fit exact number of characters.
+/// </summary>
+public class AutoSizeTextBox : TextBox {
+    static AutoSizeTextBox() {
+        DefaultStyleKeyProperty.OverrideMetadata(
+            typeof(AutoSizeTextBox),
+            new FrameworkPropertyMetadata(typeof(AutoSizeTextBox)));
+        FontSizeProperty.OverrideMetadata(typeof(AutoSizeTextBox),
+            new FrameworkPropertyMetadata(OnFontSizeChanged));
+    }
+
+    static void OnFontSizeChanged(DependencyObject d, DependencyPropertyChangedEventArgs e) {
+        ((AutoSizeTextBox)d).recalculateWidth();
+    }
+
+    public static readonly DependencyProperty LineWidthProperty = DependencyProperty.Register(
+        nameof(LineWidth),
+        typeof(Int32),
+        typeof(AutoSizeTextBox),
+        new PropertyMetadata(0, onLineWidthChanged));
+    static void onLineWidthChanged(DependencyObject d, DependencyPropertyChangedEventArgs e) {
+        ((AutoSizeTextBox)d).recalculateWidth();
+    }
+
+    public Int32 LineWidth {
+        get => (Int32)GetValue(LineWidthProperty);
+        set => SetValue(LineWidthProperty, value);
+    }
+
+    void recalculateWidth() {
+        this.MeasureStringWidth(new String('0', LineWidth), FontSize, AcceptsReturn);
+    }
+}
