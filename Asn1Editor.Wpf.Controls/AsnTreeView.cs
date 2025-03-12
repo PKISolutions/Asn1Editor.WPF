8056ef487252117107ae19022a930fb8a8ac7cd7
Vadims Podāns <vpodans@sysadmins.lv>
2025-03-12 13:01:16 +0200

    Created new AsnTreeView templated control in Controls project.

---
+++ Asn1Editor.Wpf.Controls/AsnTreeView.cs
@@ -0,0 +1,150 @@
+﻿#nullable enable
+using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using SysadminsLV.WPF.OfficeTheme.Toolkit.Commands;
+
+namespace Asn1Editor.Wpf.Controls; 
+
+public class AsnTreeView : TreeView {
+    static AsnTreeView() {
+        DefaultStyleKeyProperty.OverrideMetadata(
+            typeof(AsnTreeView),
+            new FrameworkPropertyMetadata(typeof(AsnTreeView)));
+    }
+
+    public AsnTreeView() {
+        MouseDoubleClick += OnTreeViewDoubleClick;
+        MouseRightButtonDown += OnPreviewMouseRightButtonDown;
+        SelectedItemChanged += OnTreeViewSelectedItemChanged;
+        Drop += onTreeFileDrop;
+        ExpandAllCommand = new RelayCommand(expandAll);
+    }
+
+    #region ExpandAllCommand
+
+    public static readonly DependencyProperty ExpandAllCommandProperty = DependencyProperty.Register(
+        nameof(ExpandAllCommand),
+        typeof(ICommand),
+        typeof(AsnTreeView),
+        new PropertyMetadata(default));
+
+    public ICommand ExpandAllCommand {
+        get => (ICommand)GetValue(ExpandAllCommandProperty);
+        private set => SetValue(ExpandAllCommandProperty, value);
+    }
+
+    #endregion
+
+    #region FileDropCommand
+
+    public static readonly DependencyProperty FileDropCommandProperty = DependencyProperty.Register(
+        nameof(FileDropCommand),
+        typeof(ICommand),
+        typeof(AsnTreeView),
+        new PropertyMetadata(default));
+
+    public ICommand? FileDropCommand {
+        get => (ICommand?)GetValue(FileDropCommandProperty);
+        set => SetValue(FileDropCommandProperty, value);
+    }
+
+    #endregion
+
+    #region DoubleClickCommand
+
+    public static readonly DependencyProperty DoubleClickCommandProperty = DependencyProperty.Register(
+        nameof(DoubleClickCommand),
+        typeof(ICommand),
+        typeof(AsnTreeView),
+        new PropertyMetadata(default));
+
+    public ICommand? DoubleClickCommand {
+        get => (ICommand?)GetValue(DoubleClickCommandProperty);
+        set => SetValue(DoubleClickCommandProperty, value);
+    }
+
+    #endregion
+
+    #region DoubleClickCommandParameter
+
+    public static readonly DependencyProperty DoubleClickCommandParameterProperty = DependencyProperty.Register(
+        nameof(DoubleClickCommandParameter),
+        typeof(Object),
+        typeof(AsnTreeView),
+        new PropertyMetadata(default));
+
+    public Object? DoubleClickCommandParameter {
+        get => GetValue(DoubleClickCommandParameterProperty);
+        set => SetValue(DoubleClickCommandParameterProperty, value);
+    }
+
+    #endregion
+
+    #region SelectedItem
+
+    public new static readonly DependencyProperty SelectedItemProperty = DependencyProperty.Register(
+        nameof(SelectedItem),
+        typeof(Object),
+        typeof(AsnTreeView),
+        new PropertyMetadata(default));
+
+    public new Object? SelectedItem {
+        get => GetValue(SelectedItemProperty);
+        set => SetValue(SelectedItemProperty, value);
+    }
+
+    #endregion
+
+    #region MaxTextLength
+
+    public static readonly DependencyProperty MaxTextLengthProperty = DependencyProperty.Register(
+        nameof(MaxTextLength),
+        typeof(Int32),
+        typeof(AsnTreeView),
+        new PropertyMetadata(default));
+
+    public Int32 MaxTextLength {
+        get => (Int32)GetValue(MaxTextLengthProperty);
+        set => SetValue(MaxTextLengthProperty, value);
+    }
+
+    #endregion
+
+    void OnTreeViewDoubleClick(Object sender, MouseButtonEventArgs e) {
+        DoubleClickCommand?.Execute(DoubleClickCommandParameter);
+    }
+    void OnPreviewMouseRightButtonDown(Object sender, MouseButtonEventArgs e) {
+        TreeViewItem? treeViewItem = visualUpwardSearch(e.OriginalSource as DependencyObject);
+        if (treeViewItem != null) {
+            treeViewItem.Focus();
+            e.Handled = true;
+        }
+    }
+    void OnTreeViewSelectedItemChanged(Object sender, RoutedPropertyChangedEventArgs<Object> e) {
+        SelectedItem = e.NewValue;
+    }
+    void onTreeFileDrop(Object sender, DragEventArgs e) {
+        if (!AllowDrop) {
+            e.Handled = true;
+            return;
+        }
+        String[] file = e.Data.GetData(DataFormats.FileDrop, true) as String[];
+        if (FileDropCommand != null && file?.Length > 0) {
+            FileDropCommand.Execute(file[0]);
+            e.Handled = true;
+        }
+    }
+    void expandAll(Object o) {
+        ((TreeViewItem)Items[0]).IsExpanded = true;
+    }
+
+    static TreeViewItem? visualUpwardSearch(DependencyObject? source) {
+        while (source != null && source is not TreeViewItem) {
+            source = VisualTreeHelper.GetParent(source);
+        }
+        return source as TreeViewItem;
+    }
+}
